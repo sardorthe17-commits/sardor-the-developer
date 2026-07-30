adcd10516b044fc060aa286abf53d45e71c95663
sardorthe17 <sardorthe17@gmail.com>
2026-07-30 14:41:43 +0500

    ...

--- backend/src/module/user/user.service.ts
+++ backend/src/module/user/user.service.ts
@@ -1,11 +1,14 @@
 import { adminLogin, adminParol } from "../../common/configs/admin.config"
+import { Injectable } from "@nestjs/common";
+import { adminLogin, adminParol } from "../../common/configs/admin.config"
 
+@Injectable()
 export class isAdminService{
     async detacting(payload:any){
         const {parol, login} = payload
         console.log(parol, login);
-        if(parol != adminParol()) return {success:false, message:`Parol notog\'ri`}
-        if(login != adminLogin()) return {success:false, message:`Login notog\'ri`}
+        if(parol != adminParol()) return {success:false, message:`Parol notog\\'ri`}
+        if(login != adminLogin()) return {success:false, message:`Login notog\\'ri`}
         if(!parol || !login) return {success:false}
         return {
             success:true,
