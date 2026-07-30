1d23d8391132440d4f38783a5ea66283c87059a6
sardorthe17 <sardorthe17@gmail.com>
2026-07-30 14:07:26 +0500

    ...

--- backend/src/module/user/user.service.ts
+++ backend/src/module/user/user.service.ts
@@ -3,9 +3,9 @@ import { adminLogin, adminParol } from "../../common/configs/admin.config"
 export class isAdminService{
     async detacting(payload:any){
         const {parol, login} = payload
-
-        if(parol !== adminParol()) return {success:false, message:`Parol notog\'ri`}
-        if(login !== adminLogin()) return {success:false, message:`Login notog\'ri`}
+        console.log(parol, login);
+        if(parol != adminParol()) return {success:false, message:`Parol notog\'ri`}
+        if(login != adminLogin()) return {success:false, message:`Login notog\'ri`}
         if(!parol || !login) return {success:false}
         return {
             success:true,
