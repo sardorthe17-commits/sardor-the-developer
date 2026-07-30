8b2b603c7433a4da1546b1209f75abb7cd7cc5c8
sardorthe17 <sardorthe17@gmail.com>
2026-07-30 16:03:26 +0500

    ...

--- backend/src/module/user/user.service.ts
+++ backend/src/module/user/user.service.ts
@@ -6,8 +6,8 @@ export class isAdminService{
     async detacting(payload:any){
         const {parol, login} = payload
         console.log(parol, login);
-        if(parol != adminParol()) return {success:false, message:`Parol notog\\'ri`}
-        if(login != adminLogin()) return {success:false, message:`Login notog\\'ri`}
+        if(parol != adminParol()) return {success:false, message:`Parol notog\'ri`}
+        if(login != adminLogin()) return {success:false, message:`Login notog\'ri`}
         if(!parol || !login) return {success:false}
         return {
             success:true,
